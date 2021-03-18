159fcf24e5d62cf10f07e731c625745942e8b19d
Alejandro Cen <alex96.cen@gmail.com>
2021-03-18 12:49:39 -0600

    Change require forwardable

--- test/http_client_helper.rb
+++ test/http_client_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'forwardable'
+
 class HttpClientHelper
   extend Forwardable
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'byebug'
-require 'forwardable'
 require 'minitest/autorun'
 require_relative '../lib/openpay'
 require_relative 'http_client_helper'
