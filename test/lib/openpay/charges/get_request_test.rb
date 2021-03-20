c5d7851f3000113a1297cb33085a87e51ac4fece
Alejandro Cen <alex96.cen@gmail.com>
2021-03-20 11:37:08 -0600

    Get charge by transaction id

--- test/lib/openpay/charges/get_request_test.rb
+++ test/lib/openpay/charges/get_request_test.rb
@@ -23,4 +23,13 @@ class GetRequestTest < Minitest::Test
     assert_equal(200, response.status)
     assert_equal(1, JSON.parse(response.body).count)
   end
+
+  def test_get_charges_by_transacion_id
+    transaction_id = 'trqj3v2vliza4noj0ehh'
+    request = Openpay::Charges::GetRequest.new(transaction_id)
+    response = @client.execute(request)
+
+    assert_equal(200, response.status)
+    assert_equal(transaction_id, JSON.parse(response.body)['id'])
+  end
 end
