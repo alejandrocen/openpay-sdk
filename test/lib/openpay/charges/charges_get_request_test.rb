eda9302d854db30bad50f196a6c61094de2fbe41
Alejandro Cen <alex96.cen@gmail.com>
2021-04-03 11:04:28 -0600

    Add VCR

--- test/lib/openpay/charges/charges_get_request_test.rb
+++ test/lib/openpay/charges/charges_get_request_test.rb
@@ -4,28 +4,34 @@ require_relative '../../../test_helper'
 
 class ChargesGetRequestTest < RequestTest
   def test_get_charges
-    request = Openpay::Charges::GetRequest.new
-    response = @client.execute(request)
+    VCR.use_cassette('test_get_charges') do
+      request = Openpay::Charges::GetRequest.new
+      response = @client.execute(request)
 
-    assert_equal(200, response.status)
+      assert_equal(200, response.status)
+    end
   end
 
   def test_get_charges_with_limit_param
-    request = Openpay::Charges::GetRequest.new do |req|
-      req.params = { limit: 1 }
-    end
-    response = @client.execute(request)
+    VCR.use_cassette('test_get_charges_with_limit_param') do
+      request = Openpay::Charges::GetRequest.new do |req|
+        req.params = { limit: 1 }
+      end
+      response = @client.execute(request)
 
-    assert_equal(200, response.status)
-    assert_equal(1, JSON.parse(response.body).count)
+      assert_equal(200, response.status)
+      assert_equal(1, JSON.parse(response.body).count)
+    end
   end
 
   def test_get_charges_by_transaction_id
-    transaction_id = 'trqj3v2vliza4noj0ehh'
-    request = Openpay::Charges::GetRequest.new(transaction_id)
-    response = @client.execute(request)
+    VCR.use_cassette('test_get_charges_by_transaction_id') do
+      transaction_id = 'tr6oykqjyjtwo9hia9fl'
+      request = Openpay::Charges::GetRequest.new(transaction_id)
+      response = @client.execute(request)
 
-    assert_equal(200, response.status)
-    assert_equal(transaction_id, JSON.parse(response.body)['id'])
+      assert_equal(200, response.status)
+      assert_equal(transaction_id, JSON.parse(response.body)['id'])
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
-require 'byebug'
 require 'yaml'
+require 'securerandom'
+require 'vcr'
+require 'byebug'
 require 'minitest/autorun'
 require_relative '../lib/openpay'
 require_relative './http_client_helper'
 require_relative './request_test'
+require_relative './vcr_config'
